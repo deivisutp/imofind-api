88174b0367d5f0528ef1af9877bd26f8fc6c3f22
deivisutp <deivisutp@gmail.com>
2026-08-25 23:09:33 -0300

    fix: ACRC web template

---
+++ src/test/resources/fixtures/acrc-data.js
@@ -0,0 +1,4 @@
+window.ACRC = {"provider":"ACRC Imoveis","counts":{"total":2,"sale":1,"rent":1},"listings":[
+{"id":"CA03902","transaction":"sale","price":480000,"typePt":"Casa","title":"Casa em Velha, Blumenau","city":"Blumenau","neighborhood":"Velha","address":"RUA TESTE","edificio":null,"bedrooms":3,"bathrooms":2,"suites":1,"garage":2,"livingArea":120,"lotArea":300,"images":["https://s3-sa-east-1.amazonaws.com/grupo-union/14425/casa1.jpg"],"imageCount":10},
+{"id":"AP10902","transaction":"rent","price":2100,"typePt":"Apartamento","title":"Apartamento em Vila Nova, Blumenau","city":"Blumenau","neighborhood":"Vila Nova","address":"RUA Y","bedrooms":1,"suites":1,"garage":2,"livingArea":50,"images":[],"imageCount":0}
+]};
